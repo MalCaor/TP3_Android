b8d20c1c33561cd2793476541c54f1a45dddc508
MalCaor <xav.lemen@orange.fr>
2019-12-17 14:12:36 +0100

    merci theo

--- app/src/main/java/com/example/tp3/MainActivity.java
+++ app/src/main/java/com/example/tp3/MainActivity.java
@@ -22,8 +22,8 @@ import android.net.Uri;
 
 
 public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener {
-private Fragment leFragment;
-private ImageView limage;
+    private Fragment leFragment;
+    private ImageView limage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,28 +32,28 @@ private ImageView limage;
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+/*        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         leFragment = (Fragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment);
         leFragment.getView().setVisibility(View.GONE);
-        limage= (ImageView) findViewById(R.id.imageView);
-        Button bOk=(Button) leFragment.getView().findViewById(R.id.bFragOk);
+        //limage= (ImageView) findViewById(R.id.imageView);
+        Button bOk = (Button) leFragment.getView().findViewById(R.id.bFragOk);
         bOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),    "clic sur ok", Toast.LENGTH_SHORT
+                Toast.makeText(getApplicationContext(), "clic sur ok", Toast.LENGTH_SHORT
                 ).show();
             }
         });
-        Button bCancel=(Button) leFragment.getView().findViewById(R.id.bFragCancel);
+        Button bCancel = (Button) leFragment.getView().findViewById(R.id.bFragCancel);
         bCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -74,34 +74,35 @@ private ImageView limage;
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_connect:
-                Toast.makeText(getApplicationContext(),    "clic sur connect",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "clic sur connect", Toast.LENGTH_LONG).show();
 
                 leFragment.getView().setVisibility(View.VISIBLE);
                 limage.setVisibility(View.GONE);
                 return true;
             case R.id.menu_deconnect:
-                Toast.makeText(getApplicationContext(),    "clic sur deconnect",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "clic sur deconnect", Toast.LENGTH_LONG).show();
 
                 return true;
             case R.id.menu_list:
-                Toast.makeText(getApplicationContext(),    "clic sur list",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "clic sur list", Toast.LENGTH_LONG).show();
 
                 return true;
             case R.id.menu_import:
-                Toast.makeText(getApplicationContext(),    "clic sur import",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "clic sur import", Toast.LENGTH_LONG).show();
 
                 return true;
             case R.id.menu_export:
 
-                Toast.makeText(getApplicationContext(),    "clic sur export",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "clic sur export", Toast.LENGTH_LONG).show();
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
-    }
+        }
 
+    }
     @Override
-    public void onFragmentInteraction(Uri uri) {
+    public void onFragmentInteraction (Uri uri){
 
     }
 }
